5023e36bc3509ffab5021b8727ceeec0d3927ce4
unknown <adzghan@gmail.com>
2026-06-04 15:49:56 +0700

    update logo di bagian title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Poly Games Cafe')</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/Logo_poly.jpeg') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;900&family=Roboto+Mono:wght@400;700&display=swap"
